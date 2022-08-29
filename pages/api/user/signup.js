a64b28ec4d4f4905d6461acc001fdd1e6df4a6c7
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-08-28 21:09:20 -0300

    Adicionado Joi e validação no Signup

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,8 +1,23 @@
+import connect from 'next-connect'
+import Joi from 'joi'
+
+import validate from '../../../lib/middlewares/validation'
+
 import { signupUser } from '../../../modules/user/user.service'
 
-function signup(req, res) {
-  signupUser()
+const postSchema = Joi.object({
+  firstName: Joi.string().requerid().max(50),
+  lastName: Joi.string().requerid().max(50),
+  user: Joi.string().requerid().max(30),
+  email: Joi.string().email().requerid().max(100),
+  password: Joi.string().requerid().max(50).min(6),
+
+})
+
+const signup = connnect()
+  .post( validate({body: postSchema}), (req, res) => {
+    signupUser()
   res.status(200).json({ teste:"ok" })
-}
+  })
 
 export default signup
